1390b9c97a6f096eef0747e6e4c23012b984d821
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-01-20 11:18:05 +0330

    https://github.com/farzanmosayyebi/ExtensibleFileSearcher -

---
+++ ExtensibleFileSearcher/FileSearcher/History/DatabaseManager/HistoryDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearcher
+{
+    public class HistoryDbContextFactory : IDesignTimeDbContextFactory<HistoryDbContext>
+    {
+        public HistoryDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<HistoryDbContext> optionsBuilder = new DbContextOptionsBuilder<HistoryDbContext>();
+
+            string historyFilePath = Path.Combine(Directory.GetCurrentDirectory(), "History.db");
+            optionsBuilder.UseSqlite($"Filename=History.db");
+            
+            return new HistoryDbContext(optionsBuilder.Options);
+        }
+    }
+}
